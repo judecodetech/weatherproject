f65b6c0f7a5c084b2a6f82beb12a84aca0ba2023
Jude <judeconnectionz@gmail.com>
2017-04-21 17:03:23 +0200

    Full refactor

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -1,17 +1,22 @@
 from django.conf.urls import include, url
+from django.contrib.auth import views as auth_view
+from django.views.generic import TemplateView
 
 from weatherapp import views
-from django.contrib.auth import views as auth_view
+from weatherapp.views import(
+	ForecastWeatherView, LogoutView, 
+	RegisterFormView, RegisterSuccessView
+)
 
 urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
+    url(r'^$', TemplateView.as_view(template_name='weatherapp/homepage.html'), name='homepage'),
     url(r'^login/$', auth_view.login, name='login'),
-    url(r'^logout/$', views.log_user_out, name='logout'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 
     # If user is not logged in, it will redirect to login page
     url(r'^accounts/login/$', auth_view.login),
 
-    url(r'^register/$', views.register, name='register'),
-    url(r'^register/success/$', views.register_success, name='successful_registration'),
-    url(r'^weather_forecast/$', views.forecast_weather, name='weather_forecast'),
+    url(r'^register/$', RegisterFormView.as_view(), name='register'),
+    url(r'^register/success/$', RegisterSuccessView.as_view(), name='successful_registration'),
+    url(r'^weather_forecast/$', ForecastWeatherView.as_view(), name='weather_forecast'),
 ]
