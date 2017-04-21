16e954ebbd6d601644caa51f75d3c5d2a385b266
Jude <judeconnectionz@gmail.com>
2017-04-21 10:52:07 +0200

    Modify setup description

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='weatherproject',
     version='0.1',
     packages=['weatherproject',],
+    author = "Jude Ugbefu",
+    author_email = "judeconnectionz@gmail.com",
+    description = ("This project collects weather data from weather.news24 "
+                        "and displays this data to a user."),
     license='None',
     long_description=open('README.md').read(),
 )
